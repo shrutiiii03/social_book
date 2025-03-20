68b9bab77160707a62afb0e2adf396b95edc5e94
shrutiiii03 <145834046+shrutiiii03@users.noreply.github.com>
2025-03-20 15:41:17 +0530

    Update settings.py

--- social_book/settings.py
+++ social_book/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "shrutishetty2003@gmail.com"
-EMAIL_HOST_PASSWORD = "frtx babc tqjy rujd"
+EMAIL_HOST_USER = "Your_Email"
+EMAIL_HOST_PASSWORD = "your_app_password"
